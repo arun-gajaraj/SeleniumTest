b7ec50fb07f3f847269bbe9e94bb6abfa3940b3b
Arun Gajraj <arunamigo@live.com>
2016-07-07 13:32:27 +0530

    New Files added

---
+++ FrameworkPomLight/Framework/Operate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace Framework
+{
+    class Operate
+    {
+        internal void Setup(FirefoxDriver driver)
+        {            
+            driver.Navigate().GoToUrl("http://login.live.com");
+            driver.Manage().Window.Maximize();
+        }
+
+        internal void TearDown(FirefoxDriver driverTD)
+        {
+            driverTD.Quit();
+        }
+    }
+}
---
+++ FrameworkPomLight/Framework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace Framework
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var driver = new FirefoxDriver();
+            Operate op = new Operate();
+            op.Setup(driver);
+            
+
+            op.TearDown(driver);
+
+        }
+    }
+}
